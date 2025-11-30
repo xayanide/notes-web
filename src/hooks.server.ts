bf867b7f5514174c5989af13f000ed90772891d4
xayanide <xayanide@gmail.com>
2025-11-30 09:52:10 +0800

    fix: wrong hook placement and fix server hook for refresh token

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { type Handle } from "@sveltejs/kit";
+import * as cookie from "cookie";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const accessToken = event.cookies.get("access_token");
+  const cookies = cookie.parse(event.request.headers.get("cookie") || "");
+  const accessToken = cookies.access_token;
   if (!accessToken) {
     const res = await event.fetch("/api/v1/auth/refresh", { method: "POST" });
     if (!res.ok) {
