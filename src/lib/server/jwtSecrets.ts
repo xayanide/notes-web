94586e117efbc4cfcf4a6caf4aa62fcdca386505
xayanide <xayanide@gmail.com>
2025-11-30 08:35:48 +0800

    refactor: reuse textEncoder instance

--- src/lib/server/jwtSecrets.ts
+++ src/lib/server/jwtSecrets.ts
@@ -1,4 +1,6 @@
 import { JWT_ACCESS_SECRET, JWT_REFRESH_SECRET } from "$env/static/private";
 
-export const ACCESS_SECRET = new TextEncoder().encode(JWT_ACCESS_SECRET);
-export const REFRESH_SECRET = new TextEncoder().encode(JWT_REFRESH_SECRET);
+const textEncoder = new TextEncoder();
+
+export const ACCESS_SECRET = textEncoder.encode(JWT_ACCESS_SECRET);
+export const REFRESH_SECRET = textEncoder.encode(JWT_REFRESH_SECRET);
