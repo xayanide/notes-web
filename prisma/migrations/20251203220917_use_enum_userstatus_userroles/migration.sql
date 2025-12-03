fa98ed55dc5cf473854d8b45d50c944694e1d320
xayanide <xayanide@gmail.com>
2025-12-04 06:09:29 +0800

    refactor: actually use the enums lol
    BREAKING CHANGE:

---
+++ prisma/migrations/20251203220917_use_enum_userstatus_userroles/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `role` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `status` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "role",
+ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'REGULAR',
+DROP COLUMN "status",
+ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'PENDING';
